672f664078499bd1da436be60cc375a44409a24e
Nicolo Contin <nicocontin@gmail.com>
2023-05-18 16:39:58 +0200

    done backend auth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
+const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User.model");
+const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 const router = express.Router();
 /* 
@@ -56,8 +57,16 @@ router.post("/login", async (req, res) => {
   }
 });
 
-// GET  /auth/verify
+// GET  /auth/verify  -  Used to verify JWT stored on the client
 
+router.get("/verify", isAuthenticated, (req, res, next) => {
+  // If JWT token is valid the payload gets decoded by the
+  // isAuthenticated middleware and made available on `req.payload`
+  console.log(`req.payload`, req.payload);
 
+  // Send back the object with user data
+  // previously set as the token payload
+  res.status(200).json(req.payload);
+});
 
 module.exports = router;
