05034415b30071c201301f4049f99574cba4c42f
oguzzhany <oguzzhan@me.com>
2023-05-19 02:05:53 +0200

    first commit

--- middleware/jwt.middleware.js
+++ middleware/jwt.middleware.js
@@ -6,14 +6,17 @@ const { expressjwt: jwt } = require("express-jwt");
 const isAuthenticated = jwt({
   secret: process.env.TOKEN_SECRET,
   algorithms: ["HS256"],
-  requestProperty: "payload",
+  requestProperty: "pizza",
   getToken: getTokenFromHeaders,
 });
 
 // Function used to extracts the JWT token from the request's 'Authorization' Headers
 function getTokenFromHeaders(req) {
   // Check if the token is available on the request Headers
-  if (req.headers.authorization && req.headers.authorization.split(" ")[0] === "Bearer") {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  ) {
     // Get the encoded token string and return it
     const token = req.headers.authorization.split(" ")[1];
     return token;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -11,7 +11,7 @@ const saltRounds = 10; */
 // POST  /auth/signup
 
 router.post("/signup", async (req, res, next) => {
-  const { username, password } = req.body;
+  // const { username, password } = req.body;
 
   // Generate the salt
   const salt = bcryptjs.genSaltSync(13);
@@ -27,12 +27,12 @@ router.post("/signup", async (req, res, next) => {
 
 // POST  /auth/login
 router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  // Check if username or password are provided as empty string
-  if (username === "" || password === "") {
-    res.status(400).json({ message: "Provide email and password." });
-    return;
-  }
+  // const { username, password } = req.body;
+  // // Check if username or password are provided as empty string
+  // if (username === "" || password === "") {
+  //   res.status(400).json({ message: "Provide email and password." });
+  //   return;
+  // }
 
   // Does user exists
   const potentialUser = await User.findOne({ username: req.body.username });
@@ -40,12 +40,18 @@ router.post("/login", async (req, res) => {
     // Is the password correct
     if (bcryptjs.compareSync(req.body.password, potentialUser.password)) {
       // Password IS correct
-      const authToken = jwt.sign({ userId: potentialUser._id }, process.env.TOKEN_SECRET, {
-        algorithm: "HS256",
-        expiresIn: "6h",
-      });
+      const authToken = jwt.sign(
+        { userId: potentialUser._id },
+        process.env.TOKEN_SECRET,
+        {
+          algorithm: "HS256",
+          expiresIn: "6h",
+        }
+      );
       // Send the token as the response
-      res.status(200).json({ authToken: authToken });
+      // res.status(200).json({ authToken: authToken });
+      console.log("user logged in");
+      res.json(authToken);
     } else {
       // Password ISN'T correct
       res.status(401).json({ message: "Unable to authenticate the user" });
@@ -59,14 +65,17 @@ router.post("/login", async (req, res) => {
 
 // GET  /auth/verify  -  Used to verify JWT stored on the client
 
-router.get("/verify", isAuthenticated, (req, res, next) => {
-  // If JWT token is valid the payload gets decoded by the
-  // isAuthenticated middleware and made available on `req.payload`
-  console.log(`req.payload`, req.payload);
+router.get("/verify", isAuthenticated, async (req, res, next) => {
+  // // If JWT token is valid the payload gets decoded by the
+  // // isAuthenticated middleware and made available on `req.payload`
+  // console.log(`req.payload`, req.payload);
 
-  // Send back the object with user data
-  // previously set as the token payload
-  res.status(200).json(req.payload);
+  // // Send back the object with user data
+  // // previously set as the token payload
+  // res.status(200).json(req.payload);
+
+  const user = await User.findById(req.pizza.userId);
+  res.status(200).json({ message: "User is authenticated", user });
 });
 
 module.exports = router;
